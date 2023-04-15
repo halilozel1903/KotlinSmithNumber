55537463e1ee0674dc1ff49df79eae761795df62
Halil Özel <halilozel1903@gmail.com>
2023-04-16 00:45:24 +0300

    Smith Number example added.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,46 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val start = 1
+    val end = 100
+    println("Smith numbers between $start and $end:")
+    for (i in start..end) {
+        if (isSmithNumber(i)) {
+            println(i)
+        }
+    }
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun isSmithNumber(n: Int): Boolean {
+    if (n < 4) {
+        return false
+    }
+    val digitsSum = sumOfDigits(n)
+    val factorsSum = primeFactors(n).map { sumOfDigits(it) }.sum()
+    return digitsSum == factorsSum
+}
+
+fun primeFactors(n: Int): List<Int> {
+    var num = n
+    val factors = mutableListOf<Int>()
+    var i = 2
+    while (i <= num / i) {
+        while (num % i == 0) {
+            factors.add(i)
+            num /= i
+        }
+        i++
+    }
+    if (num > 1) {
+        factors.add(num)
+    }
+    return factors
+}
+
+fun sumOfDigits(n: Int): Int {
+    var num = n
+    var sum = 0
+    while (num > 0) {
+        sum += num % 10
+        num /= 10
+    }
+    return sum
 }
