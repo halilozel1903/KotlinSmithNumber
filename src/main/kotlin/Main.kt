2fa265046cc02638b9bf3d528a058ee2a9cf4efc
Halil Ozel <halilozel1903@gmail.com>
2023-11-05 17:45:32 +0300

    Less code added.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,28 @@
 fun main() {
-    val start = 1
-    val end = 100
-    println("Smith numbers between $start and $end:")
-    for (i in start..end) {
-        if (isSmithNumber(i)) {
-            println(i)
-        }
-    }
+    val range = 1..100
+    println("Smith numbers between ${range.first} and ${range.last}:")
+    range.filter { isSmithNumber(it) }.forEach(::println)
 }
 
 fun isSmithNumber(num: Int): Boolean {
-    if (num < 4) {
-        return false
-    }
-    val digitsSum = sumOfDigits(num)
-    val factorsSum = primeFactors(num).sumOf { sumOfDigits(it) }
-    return digitsSum == factorsSum
+    return if (num < 4) false
+    else sumOfDigits(num) == primeFactors(num).sumBy { sumOfDigits(it) }
 }
 
 fun primeFactors(n: Int): List<Int> {
     var number = n
     val factors = mutableListOf<Int>()
     var i = 2
-    while (i <= number / i) {
+    while (i <= number) {
         while (number % i == 0) {
             factors.add(i)
             number /= i
         }
         i++
     }
-    if (number > 1) {
-        factors.add(number)
-    }
     return factors
 }
 
 fun sumOfDigits(n: Int): Int {
-    var num = n
-    var sum = 0
-    while (num > 0) {
-        sum += num % 10
-        num /= 10
-    }
-    return sum
+    return n.toString().sumBy { it - '0' }
 }
